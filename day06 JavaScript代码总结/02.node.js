7090be530f585640202026567fa71e4c5c4e2a8f
silence-dream <1428482231@qq.com>
2019-09-23 22:34:00 +0800

    提交:作业

---
+++ day06 JavaScript代码总结/02.node.js
@@ -0,0 +1,5 @@
+// console.log("hello world");
+
+// console.log("记得一定要打分号不然会出不知名的的bug的");
+
+console.log("加分号可以让电脑不用浪费时间判断的写的代码是否完结");
---
+++ day06 JavaScript代码总结/04.定于变量.js
@@ -0,0 +1,16 @@
+var name = 10;//这是一个赋值方法
+var na;
+na = 20; //这也是一个命名方法
+
+
+var oil;
+oil = 6;
+var pi = 300;
+
+const CL;//定义一个常量
+
+const CLL=80;
+
+
+//一定要记得加分号
+
