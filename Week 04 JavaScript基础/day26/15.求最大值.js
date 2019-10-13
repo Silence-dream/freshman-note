5d63d9342d77196c1cf676d462831482741b20ec
Silence-dream <1428482231@qq.com>
2019-10-13 21:00:02 +0800

    修改：作业订正

--- Week 04 JavaScript基础/day26/15.求最大值.js
+++ Week 04 JavaScript基础/day26/15.求最大值.js
@@ -1,11 +1,11 @@
 // 15. 求数组中的最大值和最小值, var arr =  [11, 32，55, 47，79，23]	
 
-var arr=[11,32,55,47,79,23];
-var max=arr[0];
-
-for(var i=0;i<arr.length;i++)
-{
-    var cur=arr[i];
-    cur>max? max=cur :null
+var arr = [11, 32, 55, 47, 79, 23];
+var max = arr[0];
+var min = arr[0];
+for (var i = 0; i < arr.length; i++) {
+    var cur = arr[i];
+    cur > max ? max = cur : null;
+    cur < min ? min = cur : null;
 }
-console.log(max)
+console.log(`最大值${max},最小值${min}`);
