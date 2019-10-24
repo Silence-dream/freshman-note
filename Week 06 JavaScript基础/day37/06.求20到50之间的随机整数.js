3d8e93dfd000740572327d11c0a838fb078b8a42
Silence-dream <1428482231@qq.com>
2019-10-24 21:15:51 +0800

    提交：2019.10.24作业

---
+++ Week 06 JavaScript基础/day37/06.求20到50之间的随机整数.js
@@ -0,0 +1,4 @@
+// 6. 求 20 到 50 之间的随机整数 [包括20 和 50]
+
+let num20_50 = parseInt(Math.random() * (50 - 20) + 20)
+console.log(num20_50);
---
+++ Week 06 JavaScript基础/day37/代码练习/01.waht is this.js
@@ -0,0 +1,17 @@
+// var name = "The window";
+// var object = {
+//     name: "obj",
+//     getNameFunc: function () {
+//         return function () {
+//             return this.name;
+//         }
+//     }
+// }
+// console.log(object.getNameFunc()())
+
+function Person(name, age) {
+    this.name;
+    this.age
+}
+var o = new Person("张三", 18)
+console.log(o)
