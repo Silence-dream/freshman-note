00f725a036d357d74ca2caaa02973c215c7a67d0
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-03-06 16:15:17 +0800

    修复bug

--- 网课/复习/6.js
+++ 网课/复习/6.js
@@ -1,26 +1,26 @@
-// 5. 判分小程序： 学生答案var str = "a#ab#c#D#a", 正确答案var ans = "A#AC#C#BD#B", 表示一共5题， 答对一题3分
-// 定义函数， 接收学生答案和正确答案， 计算学生得分并返回分数
-
-//  学生答案
-var str = "a#ab#c#D#a"
-// 正确答案
-var ans = "A#AC#C#BD#B"
-
-function getTureOrFalse(str, ans) {
-    // 转数组
-    // 转大写
-    let aStr = str.toUpperCase().split("#");
-    let aAns = ans.split("#");
-    // 定义分数
-    let sum = 0;
-    // console.log(str.toUpperCase().split("#"))
-    // console.log(ans.split("#"))
-    for (let i = 0; i < aAns.length; i++) {
-        if (aStr[i] == aAns[i]) {
-            sum += 3;
+function getPlus(str) {
+    let arr = str.split("");
+    // 定义结果 
+    let result = [];
+    // console.log(str.split(""))
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= "0" && arr[i] <= "9") {
+            // 定义空字符串
+            let num = "";
+            // i后面全是数字进行拼接
+            for (let j = i; j < arr.length; j++) {
+                num += arr[j];
+            }
+            // 最后+1
+            let an = Number(num) + 1;
+            result.push(an)
+            break;
+        } else {
+            // 不是数字的先存入数组
+            result.push(arr[i]);
         }
     }
-    return sum;
+    return result.join("");
 }
 
-console.log(getTureOrFalse(str, ans));
+console.log(getPlus("ATD529"));
