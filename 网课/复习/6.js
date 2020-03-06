ef4396d526cc9bf7e4d181e28e1395aa7b373f98
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-03-06 14:09:21 +0800

    添加作业

--- 网课/复习/6.js
+++ 网课/复习/6.js
@@ -1,13 +1,26 @@
-var str = 'abCDefG';
-var large = 0;
-var small = 0;
-for (let char of str) {
-    if (char == char.toUpperCase()) {
-        large++
-    } else {
-        small++
+// 5. 判分小程序： 学生答案var str = "a#ab#c#D#a", 正确答案var ans = "A#AC#C#BD#B", 表示一共5题， 答对一题3分
+// 定义函数， 接收学生答案和正确答案， 计算学生得分并返回分数
+
+//  学生答案
+var str = "a#ab#c#D#a"
+// 正确答案
+var ans = "A#AC#C#BD#B"
+
+function getTureOrFalse(str, ans) {
+    // 转数组
+    // 转大写
+    let aStr = str.toUpperCase().split("#");
+    let aAns = ans.split("#");
+    // 定义分数
+    let sum = 0;
+    // console.log(str.toUpperCase().split("#"))
+    // console.log(ans.split("#"))
+    for (let i = 0; i < aAns.length; i++) {
+        if (aStr[i] == aAns[i]) {
+            sum += 3;
+        }
     }
-    console.log();
+    return sum;
 }
-console.log(str.toLowerCase() + ';' + large + '次大写');
-console.log(str.toUpperCase() + ';' + small + '次小写');
+
+console.log(getTureOrFalse(str, ans));
