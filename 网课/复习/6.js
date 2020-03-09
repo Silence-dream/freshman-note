01b775cc1397186d4e95ac8f4200ba683ddc4e80
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-03-09 19:22:02 +0800

    提交：2020.3.9 web api day01

--- 网课/复习/6.js
+++ 网课/复习/6.js
@@ -1,26 +1,56 @@
-function getPlus(str) {
-    let arr = str.split("");
-    // 定义结果 
-    let result = [];
-    // console.log(str.split(""))
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] >= "0" && arr[i] <= "9") {
-            // 定义空字符串
-            let num = "";
-            // i后面全是数字进行拼接
-            for (let j = i; j < arr.length; j++) {
-                num += arr[j];
-            }
-            // 最后+1
-            let an = Number(num) + 1;
-            result.push(an)
-            break;
+var students = [{
+        stuid: "S01",
+        name: "周夏",
+        gender: "男",
+        score: 86
+    },
+    {
+        stuid: "S02",
+        name: "郑竹",
+        gender: "男",
+        score: 77
+    },
+    {
+        stuid: "S03",
+        name: "吴兰",
+        gender: "女",
+        score: 35
+    },
+    {
+        stuid: "S04",
+        name: "李云",
+        gender: "男",
+        score: 56
+    }
+];
+
+function totalBySex(students) {
+    let manSum = 0;
+    let woManSum = 0
+    for (let i = 0; i < students.length; i++) {
+        if (students[i]["gender"] == "男") {
+            manSum++
         } else {
-            // 不是数字的先存入数组
-            result.push(arr[i]);
+            woManSum++;
         }
     }
-    return result.join("");
+    return `男生人数${manSum}\n女生人数${woManSum}`;
 }
+console.log(totalBySex(students));
 
-console.log(getPlus("ATD529"));
+
+function getMaxScore(students) {
+    let arr = [];
+    for (let i = 0; i < students.length; i++) {
+        arr.push(students[i]["score"]);
+        // console.log(students[i]["score"])
+    }
+    // 保存最大分数
+    let maxScore = Math.max(...arr);
+    for (let i = 0; i < students.length; i++) {
+        if (students[i]["score"] == maxScore) {
+            return "成绩最高的学生姓名" + students[i]["name"]
+        }
+    }
+}
+console.log(getMaxScore(students))
