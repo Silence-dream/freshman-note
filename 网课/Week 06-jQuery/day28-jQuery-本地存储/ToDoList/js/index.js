d2ae3e4e691c597486dca1ac88692efcbcd1cc28
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-04-19 18:33:16 +0800

    todolist更新

--- 网课/Week 06-jQuery/day28-jQuery-本地存储/ToDoList/js/index.js
+++ 网课/Week 06-jQuery/day28-jQuery-本地存储/ToDoList/js/index.js
@@ -3,15 +3,42 @@ $(function () {
     // - 功能1：在表单输入框输入内容，点击”添加”按钮或者点击”enter”键，输入的内容就会添加到”正在进行的任务”列表里面
     // 添加按钮
     // 数组保存传入的数据
-    let dateArr = [];
+    // localStorage.setItem("todolist", "12")
     $(".add").on("click", function () {
-        event();
+        if ($(".txt").val().trim() == "") {
+            alert("内容不能为空");
+        } else {
+            // 得到数据
+            let data = getData();
+            // console.log(data);
+            // 存储数据
+            data.push({
+                content: $(".txt").val(),
+                done: false
+            })
+            // console.log(data);
+            saveData(data);
+            load();
+        }
     });
     $(".txt").on("keypress", function (e) {
-
         // 按下回车键
         if (e.keyCode == 13) {
-            event();
+            if ($(".txt").val().trim() == "") {
+                alert("内容不能为空");
+            } else {
+                // 得到数据
+                let data = getData();
+                // console.log(data);
+                // 存储数据
+                data.push({
+                    content: $(".txt").val(),
+                    done: false
+                })
+                // console.log(data);
+                saveData(data);
+                load()
+            }
         }
     });
     // - 功能2：如果表单的内容为空，点击”添加”按钮或者点击”enter”键的时候会提示”内容不能为空”，空字符串就不会被添加道下面
@@ -19,117 +46,66 @@ $(function () {
     //   的”正在进行的任务”列表里面
 
     // - 功能3: 当点击”正在进行的任务”列表里面的某一项的复选框，选中的那一项列表就会被添加到”已经完成的任务”列表里面，而
-    $(".w ol").on("click", "input", function () {
-        // 得到点击的复选框状态
-        let flag = $(this).prop("checked")
-        // 得到点击的内容
-        let content = $(this).siblings("span").html();
-        // 得到本地存储数据的数组形式
-        let date = JSON.parse(getDate());
-
-        for (let i = 0; i < date.length; i++) {
-            if (date[i].content == content) {
-                date[i].done = flag;
-            }
-        }
-
-        saveDate(JSON.stringify(date))
+    $(".w").on("click", "input", function () {
+        // 得到点击的index修改本地存储里面对应的索引
+        let index = $(this).attr("data-index");
+        let data = getData();
+        // console.log(data);
+        data[index].done = $(this).prop("checked");
+        // 修改完后保存数据
+        saveData(data);
         load();
-
-
-        // let i = $(this).parent().index();
-        // console.log("点击的索引" + i);
-        // // 得到本地存储数据的数组形式
-        // let date = JSON.parse(getDate());
-        // // 得到要修改的索引
-        // let dateI = date.length - 1 - i;
-        // console.log(date);
-
-        // console.log("修改的" + dateI);
-
-        // // console.log(date[dateI]);
-        // // 修改点击的复选框的done
-        // date[dateI].done = flag;
-        // // console.log(date[dateI]);
-        // // 然后重新保存进存储里面
-        // saveDate(JSON.stringify(date))
-        // load();
-    });
+    })
     //   且会从“正在进行的任务”中删除这一项
 
     // - 当刷新浏览器之后，页面还是保持不变(注：之前的数据不会消失)
-    // console.log(getDate());
+    // localStorage.setItem("todolist", "12")
 
-    // 得到本地存储数据
-    function getDate() {
-        let date = localStorage.getItem("todolist");
-        if (date != null) {
-            return date
-        } else {
-            return "[]"
-        }
+    // 存储数据
+    function saveData(data) {
+        localStorage.setItem("todolist", JSON.stringify(data));
+        // console.log(localStorage.setItem("todolist", JSON.parse(data)));
     }
-    // 保存数据
 
-    function saveDate(date) {
-        localStorage.setItem("todolist", date);
+    // 得到数据
+    function getData() {
+        // 得到数据
+        let data = localStorage.getItem("todolist");
+        if (data != null) {
+            return JSON.parse(data)
+        } else {
+            return [];
+        }
     }
 
     // 渲染数据
     function load() {
-        $(".w ol").empty();
-        $(".w ul").empty();
-        // 得到本地存储数据的数组形式
-        let date = JSON.parse(getDate());
-        $(date).each(function (index, element) {
-            // 得到内容
-            let content = element.content;
-            // console.log(element.done);
-            // console.log(content);
-            // console.log(date[index].done);
-            if (date[index].done == false) {
-                $(".w ol").prepend(`
+        // 清空输入框
+        $(".txt").val("");
+        // 得到数据
+        let data = getData();
+        // 创建前先清空
+        $(".w ul,.w ol").empty();
+        // 创建元素
+        $(data).each(function (index, element) {
+            // console.log(element);
+            // 已经完成
+            if (element.done) {
+                $(".w ul").prepend(`
                 <li>
-                <input type="checkbox">
-                <span>${date[index].content}</span>
-              </li>
+                    <input type="checkbox" data-index="${index}" checked>
+                    <span >${element.content}</span>
+                </li>
                 `);
             } else {
-                $(".w ul").prepend(`
-                    <li>
-                    <span>${date[index].content}</span>
-                  </li>
-                    `);
+                $(".w ol").prepend(`
+                <li>
+                    <input type="checkbox" data-index="${index}">
+                    <span >${element.content}</span>
+                </li>
+                `);
             }
         })
     }
 
-
-    // console.log($(".w ol").val());
-
-    // 添加进行中的任务
-    function event() {
-
-
-        // 得到内容
-        let a = $(".txt").val().trim();
-        if (a) {
-            $(".w ol").empty()
-            // 得到保存的内容
-            let content = $(".txt").val();
-            dateArr.push({
-                content: content,
-                done: false
-            })
-            // console.log(dateArr);
-            let date = JSON.stringify(dateArr)
-            // 把得到的数据保存
-            saveDate(date);
-            load();
-            $(".txt").val("");
-        } else {
-            alert("内容不能为空");
-        }
-
-    }
 });
