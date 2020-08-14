e0d10bbd2d44b658dc4062b7913108b9455488c7
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-08-14 18:15:39 +0800

    登陆功能完成

--- 网课/暑假作业/暑假作业2/app.js
+++ 网课/暑假作业/暑假作业2/app.js
@@ -2,14 +2,26 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 80;
-
+const session = require("express-session");
 // 配置静态目录
 app.use(express.static(path.join(__dirname, "public")));
-
+// 设置中间件接收post请求
+app.use(express.urlencoded({ extended: false }));
 // ejs配置
 app.set("views", path.join(__dirname, "views"));
 app.engine(".ejs", require("ejs").__express);
 app.set("view engine", "ejs");
+// session配置
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 10 * 1000, // 有效期，单位是毫秒
+    },
+  })
+);
 
 // 路由导入
 const adminLoginRoutes = require("./routes/adminLoginRoutes");
